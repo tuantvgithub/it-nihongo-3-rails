0ee70a4f032a34beb16a8056d623c36091fed3ef
tuantvgithub <tuan.tv.social@gmail.com>
2021-12-05 17:43:11 +0000

    Add name age to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/show'
-  root 'static_pages#home'    
+  root 'static_pages#home'
   get 'static_pages/home'
   devise_for :users
   resources :books
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_165555) do
+ActiveRecord::Schema.define(version: 2021_12_05_173406) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
